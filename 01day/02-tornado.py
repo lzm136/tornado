7bd29bbc77437250f7c2307690606f03f7eb39f0
lzm173112 <lzm173112@163.com>
2018-05-12 09:35:43 +0800

    app.application

--- 01day/02-tornado.py
+++ 01day/02-tornado.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
-print('kk')
+import tornado.web
+import tornado.ioloop
+
+
+class IndexView(tornado.web.RequestHandler):
+    def get(self):
+        self.write('oo')
+
+
+if __name__ == '__main__':
+    app = tornado.web.Application([(r'', IndexView)])
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
